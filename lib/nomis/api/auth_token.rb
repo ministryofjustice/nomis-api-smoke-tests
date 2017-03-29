8c655bb561104c2de27af6910d5123bb53654ffb
Al Davidson <apdavidson@gmail.com>
2017-03-29 09:52:57 +0100

    refactored auth_token for rubocop compliance

--- lib/nomis/api/auth_token.rb
+++ lib/nomis/api/auth_token.rb
@@ -4,24 +4,27 @@ require 'openssl'
 
 module NOMIS
   module API
+    # Encapsulates the process of generating a bearer token for the API gateway
     class AuthToken
       attr_accessor :client_token, :client_key, :iat_fudge_factor
 
-      # iat_fudge_factor allows you to correct for time drift on the target server
-      # for instance, if the server time is more than 10s in the future, it will 
-      # reject any client-generated bearer tokens on the grounds of 'iat skew too large'
+      # iat_fudge_factor lets you correct for time drift on the target server
+      # for instance, if the server time is more than 10s in the future,
+      # it will reject any client-generated bearer tokens on the grounds of
+      # 'iat skew too large'
       # (basically, your timestamp in your payload is too old)
       # In that case, you can pass an iat_fudge_factor of, say, 5, to generate a
-      # timestamp tagged 5s into the future and bring it back within the acceptable
-      # range. Yes, this is a fudge until we get proper NNTP set up on the VMs
-      def initialize(params={})
-        self.client_key = OpenSSL::PKey::EC.new( params[:client_key] \
-                            || default_client_key(params)
-                          )
+      # timestamp tagged 5s into the future and bring it back within the
+      # acceptable range.
+      # Yes, this is a fudge until we get proper NNTP set up on the VMs
+      def initialize(params = {})
+        self.client_key = OpenSSL::PKey::EC.new(
+          params[:client_key] || default_client_key(params)
+        )
         self.client_token = params[:client_token] \
                           || default_client_token(params)
 
-        self.iat_fudge_factor = default_iat_fudge_factor(params)
+        self.iat_fudge_factor = default_iat_fudge_factor
       end
 
       def bearer_token
@@ -47,27 +50,31 @@ module NOMIS
       def validate_keys!
         client_pub = OpenSSL::PKey::EC.new client_key
         client_pub.private_key = nil
-        client_pub_base64 = Base64.strict_encode64(client_pub.to_der)
+        client_pub_b64 = Base64.strict_encode64(client_pub.to_der)
 
         expected_client_pub = JWT.decode(client_token, nil, nil)[0]['key']
 
-        unless client_pub_base64 == expected_client_pub
-          raise 'Incorrect private key supplied ' \
-                + '(does not match public key within token)'
-        end
+        raise bad_private_key_error unless client_pub_b64 == expected_client_pub
       end
 
       protected
 
-      def default_client_key(params={})
-        read_client_key_file(params[:client_key_file] || ENV['NOMIS_API_CLIENT_KEY_FILE'])
-      end
-      
-      def default_client_token(params={})
-        read_client_key_file(params[:client_token_file] || ENV['NOMIS_API_CLIENT_TOKEN_FILE'])
+      def bad_private_key_error
+        'Incorrect private key supplied ' \
+                + '(does not match public key within token)'
       end
 
-      def default_iat_fudge_factor(params={})
+      def default_client_key(params = {})
+        read_client_key_file(params[:client_key_file] \
+          || ENV['NOMIS_API_CLIENT_KEY_FILE'])
+      end
+      
+      def default_client_token(params = {})
+        read_client_key_file(params[:client_token_file] \
+          || ENV['NOMIS_API_CLIENT_TOKEN_FILE'])
+      end
+
+      def default_iat_fudge_factor
         ENV['NOMIS_API_IAT_FUDGE_FACTOR'].to_i || 0
       end
 
